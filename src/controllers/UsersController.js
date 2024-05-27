d148688a865f26f6df059a6e362deb6e2e89fb21
Raphael <francisco.raphaelfreitas@gmail.com>
2024-05-27 00:51:51 -0300

    creatind error handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,12 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
   create(request, response) {
     const {name, email, password} = request.body
 
+    if(!name){
+      throw new AppError("Name is mandatory!")
+    }
     response.json({name, email, password})
   }
 }
